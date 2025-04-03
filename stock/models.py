933743735131f7ee17ec8ba4a547d42d4bb905e2
DanieSemu <danielsemu06@gmail.com>
2025-04-03 21:09:06 +0300

    start creating table

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField( max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class WareHouse(models.Model):
+    name=models.CharField(max_length=150)
+    location=models.CharField(max_length=150)    
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Supplier(models.Model):
+    name=models.CharField( max_length=50)
+    phone_number=models.CharField( max_length=50)
+    email=models.EmailField( max_length=254)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class StockItem(models.Model):
+    name=models.CharField(max_length=100)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity=models.IntegerField(blank=True)
+    price=models.DecimalField(blank=True)
+    supplier=models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    warehouse=models.ForeignKey(WareHouse, on_delete=models.CASCADE)
+    min_stock_level=models.IntegerField(blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
